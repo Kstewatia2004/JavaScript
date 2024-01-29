2021470c756bc38db464248d8f2e797e5100c1cb
Kstewatia2004 <153065518+Kstewatia2004@users.noreply.github.com>
2024-01-29 09:12:22 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("ram");
+"use strict"//uses newer version js
+
+const accountId=19889;
+let accountEmail="ram19889@gmail.com";
+var accountPerformance="A";
+// avoid using var as it causes problem in the block scope and the functional scope
+
+console.table([accountId,accountEmail,accountPerformance]);
+
+/* NULL is a stand alone data type for eg. if the program returns 0 for a temperature
+ program then we will not want 0 as it itself is a temp instead we will want NULL*/
+
+ console.log(typeof null)// object
