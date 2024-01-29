92b6f500d6bd23184a7c810a93c1aae921a2c00e
Kstewatia2004 <153065518+Kstewatia2004@users.noreply.github.com>
2024-01-29 16:55:40 +0530

    Update DataTypes.js

--- 01_basics/DataTypes.js
+++ 01_basics/DataTypes.js
@@ -11,3 +11,13 @@ console.table([accountId,accountEmail,accountPerformance]);
  program then we will not want 0 as it itself is a temp instead we will want NULL*/
 
  console.log(typeof null)// object
+
+ let Num= "33abc";
+ let valueInNumber= Number(Num);
+ console.log(valueInNumber);
+
+//  if you try to convert between the datatypes javascript will convert it although it may not be correct
+
+let converBool=Boolean(Num);
+console.log(converBool);
+// if you convert an empty string into boolean it wiil give false but if the string is not empty then it will give true
