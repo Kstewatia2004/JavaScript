145e29520b4a36136a682364d353dff17354e058
Kstewatia2004 <153065518+Kstewatia2004@users.noreply.github.com>
2024-02-15 18:06:46 +0000

    csll_stack

---
+++ 01_basics/call_stack.js
@@ -0,0 +1,16 @@
+/* 
+Whenever a code is executed in javascript a GLOBAL EXECUTION CONTEXT(GEC) gets created
+GEE is the first stage in the execution of javascript.
+The GEE that is created gets referred to by "this" variable
+The GEE of the browser is different that that of the node.js environment*/
+
+/*The other execution context that are created are the:-
+> FUNCTIONAL EXECUTION CONTEXT
+>EVAL EXECUTION CONTEXT*/
+
+/* A code in javascript is executed in two phases:-
+>Memory Creation Phase
+>Execution Phase*/
+
+/* Call Stack is a stck in which the bottom element is the GEE
+In Call Stack methods are executed in the LIFO format
